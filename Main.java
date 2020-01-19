3d6ec4c3a3daff9c94c3528f6e7bb986b609633e
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-01-19 18:00:32 -0300

    Traversing
    Added Traversal Class and examples

--- Main.java
+++ Main.java
@@ -4,13 +4,24 @@ public class Main {
 	public static void main(String[] args) {
 	
 		Graph graph = new Graph();
+		graph.addVertex("Bob");
+		graph.addVertex("Alice");
+		graph.addVertex("Mark");
+		graph.addVertex("Rob");
+		graph.addVertex("Maria");
+		graph.addEdge("Bob", "Alice");
+		graph.addEdge("Bob", "Rob");
+		graph.addEdge("Alice", "Mark");
+		graph.addEdge("Rob", "Mark");
+		graph.addEdge("Alice", "Maria");
+		graph.addEdge("Rob", "Maria");
 		
 		
+		System.out.println(graph.printGraph());		
 		
+		System.out.println(Traversal.depthFirstTraversal(graph, "Rob"));
 		
-		
-		System.out.println(graph.createGraph().printGraph());
-		
+		System.out.println(Traversal.breadthFirstTraversal(graph, "Maria"));	
 
 	}
 
